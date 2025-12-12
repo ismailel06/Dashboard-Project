6716e64fb00d2a967c1697c391a419c3aa5aaec4
ismailel06 <elgoureissmail@gmail.com>
2025-12-12 01:37:48 +0100

    Last update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const bodyelement = document.querySelector("body")
 const toggle = document.getElementById("toggle")
 const toggleicon = document.querySelector(".fa-moon")
+const appendsidebar = document.querySelector(".sidebar-icon")
+const sidebar = document.querySelector(".sidebar")
 
 toggle.addEventListener("change",function(){
     if(toggle.checked){
@@ -11,5 +13,15 @@ toggle.addEventListener("change",function(){
         bodyelement.style.backgroundColor = "white"
         toggleicon.style.color = "#1b5a84"
         bodyelement.style.color = "black"
+    }
+})
+
+appendsidebar.addEventListener("click" ,function(){
+    const currwidth = getComputedStyle(sidebar).width
+
+    if(currwidth === "0px"){
+        sidebar.style.width = "250px"
+    }else{
+        sidebar.style.width = "0px"
     }
 })
